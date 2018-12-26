6cdfea2225ba7c622f1aac53b8e632fbc095c200
haoliu <haoliu@thoughtworks.com>
2018-12-26 10:59:04 +0800

    DB

--- app/app.lua
+++ app/app.lua
@@ -1,5 +1,6 @@
 local lapis = require("lapis")
 local app = lapis.Application()
+local console = require("lapis.console")
 local after_dispatch = require("lapis.nginx.context").after_dispatch
 local to_json = require("lapis.util").to_json
 
@@ -7,6 +8,8 @@ app:get("/", function()
     ngx.print("Connected! Please type stuff (empty line to stop):")
     return "Welcome to Lapis " .. require("lapis.version")
 end)
+
+app:match("/console", console.make())
 
 app:before_filter(function(self)
     after_dispatch(function()
